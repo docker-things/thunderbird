0a6ef3dd6223f982f589f9219f73ea9cd726692e
Gabriel Ionescu <gabi.ionescu+github@protonmail.com>
2019-08-21 13:42:37 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM gui-apps-base:18.04
+MAINTAINER Gabriel Ionescu <gabriel.ionescu@gameloft.com>
+
+RUN apt-get update \
+ && apt-get install -y \
+        thunderbird \
+ && apt-get clean -y \
+ && apt-get autoclean -y \
+ && apt-get autoremove -y
+
+# SET USER
+USER $DOCKER_USERNAME
+
+# ENTRYPOINT
+ENTRYPOINT /usr/bin/thunderbird
+
---
+++ config.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# The name of the docker image
+PROJECT_NAME="thunderbird"
+
+# BUILD ARGS
+BUILD_ARGS=(
+    )
+
+# LAUNCH ARGS
+RUN_ARGS=(
+    -e DISPLAY=$DISPLAY
+    -v /tmp/.X11-unix:/tmp/.X11-unix
+
+    --memory="2g"
+    --cpu-shares=1024
+
+    -v $(pwd)/data/thunderbird:/home/$(whoami)/.thunderbird
+    -v $(pwd)/data/cache:/home/$(whoami)/.cache
+
+    -v /home/$(whoami)/Documents:/home/$(whoami)/Documents
+    -v /home/$(whoami)/Downloads:/home/$(whoami)/Downloads
+
+    --rm
+    -d
+    )
